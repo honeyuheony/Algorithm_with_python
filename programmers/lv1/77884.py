c39845c5f795fd0ab5bf2698a47d0cd4d5851b5e
Kuhy <kwh1019_@naver.com>
2021-12-22 17:52:52 +0900

    Solve : level1 약수의 개수와 덧셈

---
+++ programmers/lv1/77884.py
@@ -0,0 +1,15 @@
+# programmers level1 약수의 갯수와 덧셈
+# https://programmers.co.kr/learn/courses/30/lessons/77884
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        tmp = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                tmp += 1
+        if tmp % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
