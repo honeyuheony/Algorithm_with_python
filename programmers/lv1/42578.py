d389cceb655f07a881a616138e7dbab932da97c2
Kuhy <kwh1019_@naver.com>
2021-12-22 19:25:05 +0900

    Solve : level 완주하지 못한 선수

--- programmers/lv1/42578.py
+++ programmers/lv1/42578.py
@@ -1,6 +1,9 @@
 # programmers level1 완주하지 못한 선수
 # https://programmers.co.kr/learn/courses/30/lessons/42576
 
+# 해싱과 유사한 기법을 사용
+# 문자열의 길이와 첫글자를 토대로 입력값 분류해 탐색을 효과적으로 만듦.
+# 해싱을 이용한다면 더 좋은 해결 가능
 def solution(participant, completion):
 
     hash_completion = [[[] for col in range(20)] for row in range(26)]
