70ecf56fc038b222ddcb52af40ab543f35e1ab6c
Kuhy <kwh1019_@naver.com>
2022-01-24 09:43:15 +0900

    Solve : s4 괄호

--- baekjoon/silver/9012.py
+++ baekjoon/silver/9012.py
@@ -1,16 +1,19 @@
 # baekjoon s4 괄호
 # https://www.acmicpc.net/problem/9012(
 
-# import sys
-# N = int(sys.stdin.readline())
-
-# for _ in range(N):
-#     s = sys.stdin.readline()
-#     while '()' in s:
-#         s = s.replace('()', '')
-#     sys.stdout.write('NO\n') if len(s) > 1 else sys.stdout.write('YES\n')
 import sys
 from collections import deque
+
+# 문자열 풀이
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    s = sys.stdin.readline()
+    while '()' in s:
+        s = s.replace('()', '')
+    sys.stdout.write('NO\n') if len(s) > 1 else sys.stdout.write('YES\n')
+
+# 스택 풀이
 N = int(sys.stdin.readline())
 
 for _ in range(N):
