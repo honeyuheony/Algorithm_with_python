c1b35b7cf362770de657476615961bece6a18d3d
Kuhy <kwh1019_@naver.com>
2022-03-16 20:39:36 +0900

    Solve : s1 미로 탐색

--- baekjoon/silver/2178.py
+++ baekjoon/silver/2178.py
@@ -1,5 +1,6 @@
 # baekjoon s1 미로 탐색
 # https://www.acmicpc.net/problem/2178
+# bfs 최적탐색에서 queue, temp를 이용한 풀이시 temp에 넣기전에 방문표시
 
 from collections import deque
 import sys
@@ -18,18 +19,20 @@ while flag:
     temp = deque()
     while queue:
         x, y = queue.popleft()
-        print(x, y)
         if x == N-1 and y == M-1:
             temp = []
             break
-        matrix[x][y] = 0
         if 0 < x and matrix[x-1][y]:
+            matrix[x-1][y] = 0
             temp.append((x-1, y))
         if x < N-1 and matrix[x+1][y]:
+            matrix[x+1][y] = 0
             temp.append((x+1, y))
         if 0 < y and matrix[x][y-1]:
+            matrix[x][y-1] = 0
             temp.append((x, y-1))
         if y < M-1 and matrix[x][y+1]:
+            matrix[x][y+1] = 0
             temp.append((x, y+1))
     result += 1
     if temp:
