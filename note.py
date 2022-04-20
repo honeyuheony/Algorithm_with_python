f2ac1ecb99d41e3638b99bbdc8371483886096de
Kuhy <kwh1019_@naver.com>
2022-04-20 22:16:01 +0900

    Create note.py

---
+++ note.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+# BFS
+# 1. 너비우선으로 탐색하기 위해 다음 방문할 노드를 queue에 삽입
+# 2. 중간 단계를 기록해야하는 경우 temp 사용해 한번 빠져나온 뒤 다시 queue 생성
+
+# DFS
+# 1. 깊이 우선으로 탐색하기 위해 다음 방문핧 노드를 stack에 삽입
+# 2. 보통 재귀로 구현
+
+# DP
+
+# Greedy
+
+# 브루트포스
