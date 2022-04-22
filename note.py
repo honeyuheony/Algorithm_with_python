f12dbe7148b9a942819038089a675e978c95f09c
Kuhy <kwh1019_@naver.com>
2022-04-22 15:17:10 +0900

    Update note.py

--- note.py
+++ note.py
@@ -13,3 +13,9 @@ from collections import deque
 # Greedy
 
 # 브루트포스
+
+# 이분탐색
+# 1. start = 0, end = 길이-1
+# 2. mid = (start + end) // 2
+# 3-1. mid값이 target보다 작다면 end = mid-1
+# 3-2. mid값이 target보다 크다면 start = mid + 1
