f7c39b73aacecc28e95be338429330bb8faa4d01
Kuhy <kwh1019_@naver.com>
2022-05-07 02:30:33 +0900

    Update note.py

--- note.py
+++ note.py
@@ -1,5 +1,8 @@
 from collections import deque
 
+# 트리
+
+
 # BFS
 # 1. 너비우선으로 탐색하기 위해 다음 방문할 노드를 queue에 삽입
 # 2. 중간 단계를 기록해야하는 경우 temp 사용해 한번 빠져나온 뒤 다시 queue 생성
@@ -13,6 +16,8 @@ from collections import deque
 # Greedy
 
 # 브루트포스
+
+# 백트래킹
 
 # 이분탐색
 # 1. start = 0, end = 길이-1
