8e8590e944e14574f7c226a47d3f868a64a9b454
Kuhy <kwh1019_@naver.com>
2022-06-14 23:30:23 +0900

    solve : s1 카잉달력

--- baekjoon/silver/6064.py
+++ baekjoon/silver/6064.py
@@ -1,19 +1,21 @@
-# baekjoon s1 카잉 달력
-# https://www.acmicpc.net/problem/6064
-
 import math
 import sys
-import time
 input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
-    lcm = (M*N)//math.gcd(M, N)+1
-    flag = True
-    for i in range(y, lcm+1, N):
-        if (i-x) % M == 0:
-            print(i)
-            flag = False
-    if flag:
+    threshold = M*N / math.gcd(M, N)
+    isValidMonth = False
+    year = x if M > N else y
+    temp = max(M, N)
+    while year <= threshold:
+        if year % M in (0, x) and year % N in (0, y):
+            isValidMonth = True
+            break
+        else:
+            year += temp
+    if isValidMonth:
+        print(year)
+    else:
         print(-1)
